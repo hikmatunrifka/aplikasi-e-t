c0ffce3127f99f47f249973c524d99319ae035fd
hikmatunrifka <rifka09@mhs.unsyiah.ac.id>
2023-05-05 22:25:59 +0700

    ubah tampilan

---
+++ newpost.php
@@ -0,0 +1,37 @@
+<?php
+include'navbar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi e-T</title>
+    <link rel="stylesheet" href="style/newpost.css">
+</head>
+<body>
+
+    <!-- untuk konten 1 -->
+    <div class="wrapperKonten">
+      <div class="menuKonten">
+        <a class="logoShop" href=""><a class="deskripsi" href=""> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+          <a class="logoShop" href="">New Post</a>
+          <a class="logoShop" href="">Bagikan</a>
+        </div></div>
+        <center><p class="img">
+            <a href="">+</center></a></div>
+    <div>
+        <p class="deskripsi">Tulis Nama Produk <input type="text" id="namaProduk"> 
+        <p class="deskripsi">Harga <input type="Tulis Nama Produk" id="namaProduk">
+        <p class="deskripsi">Stock <input type="Tulis Nama Produk" id="namaProduk"> 
+        <p></p>
+        <p class="deskripsi">Deskripsi</p>
+        <p class="deskripsi"><input type="Tulis Nama Produk" id="namaProduk"> 
+    </div>
+
+</body>
+</html>
