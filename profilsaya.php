379e308bd5330d52f997f680334fd3167403f1c7
Taufiq <taufiqur1@mhs.unsyiah.ac.id>
2023-05-12 17:53:20 +0700

    tamabah profil user

---
+++ profilsaya.php
@@ -0,0 +1,70 @@
+<?php
+include'navbar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Profile Page</title>
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    />
+    <!-- CSS -->
+    <link rel="stylesheet" href="style/profilsaya.css" />
+  </head>
+  <body>
+    <div class="header__wrapper">
+      <header></header>
+      <div class="cols__container">
+        <div class="left__col">
+          <div class="img__container">
+            <img src="foto/bunga.jpg" alt="Dehya" />
+            <span></span>
+          </div>
+          <h2>Dehya</h2>
+          <p>Pengrajin</p>
+          <p>Dehya@gmail.com</p>
+
+          <ul class="about">
+            <li><span>6</span>Post</li>
+            <li><span>322</span>Pengikut</li>
+            <li><span>500</span>Mengikuti</li>
+          </ul>
+
+          <div class="content">
+            <p>
+              Mencari Berkah
+            </p>
+
+           
+          </div>
+        </div>
+        <div class="right__col">
+          <nav>
+            <ul>
+              <li><a href="">Post</i> </a></li>
+              <li><a href="">save</a></li>
+              <li><a href="">keterangan barang</a></li>
+            
+            </ul>
+            <button>Edit Profil</button>
+          </nav>
+
+          <div class="photos">
+            <img src="foto/bunga.jpg" alt="Photo" />
+            <img src="foto/bunga.jpg" alt=foto/bunga.jpg/.>
+            <img src="foto/bunga.jpg" alt="Photo" />
+            <img src="foto/bunga.jpg" alt="Photo" />
+            <img src="foto/bunga.jpg" alt="Photo" />
+            <img src="foto/bunga.jpg" alt="Photo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
