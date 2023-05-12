2b16f197bb947e8781b023bed16682fa90c5d797
Arya Risky Ramadhan Pratama Putra <arya_r@mhs.unsyiah.ac.id>
2023-05-12 16:28:54 +0700

    ambil konten video dari database

--- beranda.php
+++ beranda.php
@@ -1,6 +1,21 @@
 <?php
 include'navbar.php';
 ?>
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'db_e-trash';
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+    mysqli_select_db($conn, $db);
+?>
+<?php
+    $query = "SELECT * FROM tb_video;";
+    $sql = mysqli_query($conn, $query);
+    $result = mysqli_fetch_row($sql);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,50 +30,24 @@ include'navbar.php';
     <title>E-Trash</title>
 </head>
 <body>
+
     <div class="container">
-    <div class="row mt-5">
-        <div class="col-md-4 mt-4">
+    <div class="row mt-5 ">
+    <?php
+        while($result = mysqli_fetch_assoc($sql)){
+    ?>
+        <div class="col-md-4 mt-4 ">
           <div class="card mb-4">
-          <img src="file_kerajinan/img1.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
+          <div class="card-header"><img src="file_kerajinan/img1.jpg" height="30" width="30"> <a href="" class="text-decoration-none text-dark">amishop</a><img src="ikon/play.svg"> </div>
+          <img src="file_kerajinan/<?php echo $result['nama_thumbnail'];?>" alt="Foto Postingan" height="250" width="300" class="card-img-top">
+          <div class="card-body">
+                <h5 class="card-title"><?php echo $result['judul_video'];?></h5></div>
           </div>
         </div>
-        <div class="col-md-4 mt-4">
-          <div class="card mb-4">
-          <img src="file_kerajinan/img2.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
-          </div>
-        </div>
-        <div class="col-md-4 mt-4">
-          <div class="card mb-4">
-          <img src="file_kerajinan/img3.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
-          </div>
-        </div>
-        <div class="col-md-4 mt-4">
-          <div class="card mb-4">
-          <img src="file_kerajinan/img1.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
-          </div>
-        </div>
-        <div class="col-md-4 mt-4">
-          <div class="card mb-4">
-          <img src="file_kerajinan/img2.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
-          </div>
-        </div>
-        <div class="col-md-4 mt-4">
-          <div class="card mb-4">
-          <img src="file_kerajinan/img3.jpg" alt="Foto Postingan" height="250" width="300" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5></div>
-          </div>
-        </div>
+        <?php
+       }
+    ?>
     </div>
+
 </body>
 </html>
