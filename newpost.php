48f85d967e83ae485fe51d0ee62f34a3c97a1382
hikmatunrifka <rifka09@mhs.unsyiah.ac.id>
2023-05-13 21:18:01 +0700

    mengubah kolom

--- newpost.php
+++ newpost.php
@@ -2,13 +2,45 @@
 include'navbar.php';
 ?>
 
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'db_e-trash');
+
+if ( isset($_POST["submit"])){
+  $namaVideo = $_POST["nama_video"];
+  $namaThumbnail = $_POST["nama_thumbnail"];
+  $judulVideo = $_POST["judul_video"];
+  $bahan = $_POST["bahan"];
+  $alat = $_POST["alat"];
+
+  $query = " INSERT INTO tb_video VALUES ('', '$namaVideo', '$namaThumbnail', '$judulVideo', '$bahan', '$alat')";
+
+  mysqli_query($conn, $query);
+
+  if( mysqli_affected_rows($conn) > 0){
+    echo "
+        <script>
+            alert('data berhasil ditambahkan!');
+            document.location.herf = 'beranda.php';
+          </script>
+      ";
+  } else "
+        <script>
+            alert('data gagal ditambahkan!');
+            document.location.herf = 'beranda.php';
+        </script>
+      ";
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-Trash</title>
-    <link rel="stylesheet" href="style/newpost.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <meta charset="UTF-8">
@@ -17,37 +49,43 @@ include'navbar.php';
 </head>
 <body>
   <div class="container mt-4">
+  <form action="" method="post">
     <h1>Post Video</h1>
     <hr class="border border-success border-2 opacity-90" >
     
     <div class="mb-3">
         <label for="formFile" class="form-label">Upload Video</label>
-        <input class="form-control" type="file" id="formFile"></div>
+        <input class="form-control" type="file" name="nama_video" id="formFile" required></div>
 
     <div class="mb-3">
-        <label for="formFile" class="form-label">Upload Thumbnail  Video</label>
-        <input class="form-control" type="file" id="formFile"></div>
+        <label for="formFile1" class="form-label">Upload Thumbnail  Video</label>
+        <input class="form-control" type="file" name="nama_thumbnail" id="formFile1" required></div>
 
     <div class="mb-3 row mt-4">
     <label  for="judul_video" class="col-sm-2 col-form-label">Judul Video</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="judul_video" placeholder="....">
+      <input type="text" class="form-control" name="judul_video" id="judul_video" placeholder="...." required>
     </div>
+
     <label for="bahan" class="col-sm-2 col-form-label">Bahan</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="bahan" placeholder="....">
+      <input type="text" class="form-control" name="bahan" id="bahan" placeholder="...." required>
     </div>
+
     <label for="alat" class="col-sm-2 col-form-label">Alat</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="alat" placeholder="....">
+      <input type="text" class="form-control" name="alat" id="alat" placeholder="...." required>
     </div>
+
     <div class="mb-3 row mt-3">
       <div type="col">
-        <button type="button" class="btn-post">Pos Video</button>
-        <a href="masukAkun.php" type="button" class="btn-cancel text-decoration-none">Batal</a>
+        <button type="submit" name="submit" class="btn btn-primary" style="background-color: #395144;">Post Video</button>
+        
+        <a href="masukAkun.php" type="button" class="btn btn-danger">Batal</a>
       </div>
 
     </div>
+    </form>
   </div>
 </body>
 </html>
--- postfoto.php
+++ postfoto.php
@@ -2,13 +2,45 @@
 include'navbar.php';
 ?>
 
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'db_e-trash');
+
+if ( isset($_POST["submit"])){
+  $namaFoto = $_POST["nama_foto"];
+  $namaBarang = $_POST["nama_barang"];
+  $dekripsiBarang = $_POST["deskripsi_barang"];
+  $harga = $_POST["harga"];
+  $stok = $_POST["stok"];
+
+  $query = " INSERT INTO tb_produk VALUES ('', '$namaFoto', '$namaBarang', '$dekripsiBarang', '$harga', '$stok')";
+
+  mysqli_query($conn, $query);
+
+  if( mysqli_affected_rows($conn) > 0){
+    echo "
+        <script>
+            alert('Produk berhasil ditambahkan!');
+            document.location.herf = 'beranda.php';
+          </script>
+      ";
+  } else "
+        <script>
+            alert('Produk gagal ditambahkan!');
+            document.location.herf = 'beranda.php';
+        </script>
+      ";
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>E-Trash</title>
-    <link rel="stylesheet" href="style/postfoto.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <meta charset="UTF-8">
@@ -17,37 +49,40 @@ include'navbar.php';
 </head>
 <body>
   <div class="container mt-4">
-    <h1>Post Foto</h1>
+  <form action="" method="post">
+    <h1>Post Produk</h1>
     <hr class="border border-success border-2 opacity-90" >
     
     <div class="mb-3">
         <label for="formFile" class="form-label">Upload Foto</label>
-        <input class="form-control" type="file" id="formFile"></div>
+        <input class="form-control" type="file" name="nama_foto" id="formFile" required></div>
 
     <div class="mb-3 row mt-4">
+
     <label  for="nama_barang" class="col-sm-2 col-form-label">Nama Barang</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="nama_barang" placeholder="....">
+      <input type="text" class="form-control" name="nama_barang" id="nama_barang" placeholder="...." required>
     </div>
-    <label for="deskripsi_barang" class="col-sm-2 col-form-label">Deskripsi Barang</label>
+
+    <label  for="deskripsi_barang" class="col-sm-2 col-form-label">Deskripsi Barang</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="deskripsi_barang" placeholder="....">
+      <input type="text" class="form-control" name="deskripsi_barang" id="deskripsi_barang" placeholder="...." required>
     </div>
+
     <label for="harga" class="col-sm-2 col-form-label">Harga</label>
     <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="harga" placeholder="....">
+      <input type="number" class="form-control" name="harga" id="harga" placeholder="...." required>
     </div>
-    <label for="stok" class="col-sm-2 col-form-label">Stok</label>
-    <div class="col-sm-10 mb-3">
-      <input type="text" class="form-control" id="stok" placeholder="....">
-    </div>
+    
     <div class="mb-3 row mt-3">
       <div type="col">
-        <button type="button" class="btn-post">Pos Foto</button>
-        <a href="masukAkun.php" type="button" class="btn-cancel text-decoration-none">Batal</a>
+        <button type="submit" name="submit" class="btn btn-primary" style="background-color: #395144;">Post Produk</button>
+        
+        <a href="masukAkun.php" type="button" class="btn btn-danger">Batal</a>
       </div>
 
     </div>
+    </form>
   </div>
 </body>
 </html>
