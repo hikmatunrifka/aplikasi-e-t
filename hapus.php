4bf97ca7636269c55f73864aa92c86509f86b72c
Arya Risky Ramadhan Pratama Putra <arya_r@mhs.unsyiah.ac.id>
2023-05-15 12:43:47 +0700

    tampah fitur hapus

---
+++ hapus.php
@@ -0,0 +1,38 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'db_e-trash';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+mysqli_select_db($conn, $db);
+$id = $_GET["id"];
+
+$result = mysqli_query($conn, "DELETE FROM tb_video WHERE id_video= '$id'");
+
+if ($result) {
+    if ( mysqli_affected_rows($conn) > 0){
+        echo "
+        <script>
+        alert('data berhasil dihapus!');
+        document.location.href= 'profilsaya.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+        alert('tidak ada data yang dihapus!');
+        document.location.href= 'profilsaya.php';
+        </script>
+        ";
+    }
+} else {
+    echo "
+    <script>
+    alert('gagal menghapus data: " . mysqli_error($conn) . "');
+    document.location.href= 'profilsaya.php';
+    </script>
+    ";
+}
+
